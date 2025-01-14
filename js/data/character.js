45370eacc8a21a1647d598bff24afa7ecc117e8e
WhiteShuriken <kemseygillot@outlook.fr>
2025-01-14 22:34:32 +0100

    Mise à jour des personnages avec de nouvelles raretés et ajustement des données de rareté

--- js/data/character.js
+++ js/data/character.js
@@ -1,6 +1,19 @@
+// Rarity: common, rare, epic, legend
 export const character = [
-    { id: 1, name: "Arnaud", rarity: "legend" },
-    { id: 2, name: "Kemsey", rarity: "epic" },
-    { id: 3, name: "Sabrina", rarity: "rare" },
-    { id: 4, name: "Nael", rarity: "common" }
+    { id: 1, name: "Khalil", rarity: "legend" },
+    { id: 2, name: "Nassim", rarity: "legend" },
+    { id: 3, name: "Mia", rarity: "epic" },
+    { id: 4, name: "Kemsey", rarity: "epic" },
+    { id: 5, name: "Axel", rarity: "epic" },
+    { id: 6, name: "Benjamin", rarity: "rare" },
+    { id: 7, name: "Mateo", rarity: "rare" },
+    { id: 8, name: "Allison", rarity: "rare" },
+    { id: 9, name: "David", rarity: "rare" },
+    { id: 10, name: "Erwin", rarity: "common" },
+    { id: 11, name: "Kilian", rarity: "common" },
+    { id: 12, name: "Nael", rarity: "common" },
+    { id: 13, name: "Arnaud", rarity: "common" },
+    { id: 14, name: "Cyril", rarity: "common" },
+    { id: 15, name: "Loic", rarity: "common" },
+    { id: 16, name: "Sabrina", rarity: "common" },
 ];
--- js/data/rarity.js
+++ js/data/rarity.js
@@ -2,5 +2,5 @@ export const rarity = [
     { id: "common", name: "Commun", probability: 50 },
     { id: "rare", name: "Rare", probability: 30 },
     { id: "epic", name: "Épique", probability: 15 },
-    { id: "legend", name: "Légendaire", probability: 5 }
+    { id: "legend", name: "Légendaire", probability: 5 },
 ];
